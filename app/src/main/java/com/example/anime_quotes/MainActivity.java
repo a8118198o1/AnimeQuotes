70c1e6aaa20debf19131749e33d30c4ac23c1a6f
a8118198o1 <heyharsh5566@gmail.com>
2025-01-18 22:18:13 +0530

    Functional code that provide string data from the api

--- app/src/main/java/com/example/anime_quotes/MainActivity.java
+++ app/src/main/java/com/example/anime_quotes/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.anime_quotes;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,15 +34,35 @@ public class MainActivity extends AppCompatActivity {
 
         //getting view by id for the btn and txt
         Button showAnimeQuotes = findViewById(R.id.btnShowQuotes);
-        TextView animeQuotes = findViewById(R.id.txtAnimeQuotes);
+        TextView txtAnimeQuotes = findViewById(R.id.txtAnimeQuotes);
 
         //making request queue for the api
         RequestQueue queue = Volley.newRequestQueue(this);
 
         //setting api of the website
-        String animeQuotesApi = "https://anime-facts-rest-api.herokuapp.com/api/v1";
+        String animeQuotesApi = "https://www.google.com/";
 
-        //
+        //setting to request queue
+        RequestQueue que = Volley.newRequestQueue(this);
+
+        //initialing onclick listener on btn
+        showAnimeQuotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest animeQuotes = new StringRequest(Request.Method.GET, animeQuotesApi, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        txtAnimeQuotes.setText(response.substring(0, 1000));
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error occur while fetching data from the api", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                que.add(animeQuotes);
+            }
+        });
 
     }
 }
