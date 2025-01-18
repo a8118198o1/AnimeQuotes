627505fab0f4047a87279f9756bd9c896f0a0982
a8118198o1 <heyharsh5566@gmail.com>
2025-01-18 21:22:52 +0530

    Added volley library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    //volley library for the api
+    implementation(libs.volley)
 }
